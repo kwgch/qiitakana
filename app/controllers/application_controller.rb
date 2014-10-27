261982d1bfc9449df56aaf877e4a7c71b382c8b9
Akihiro Matsumura <matsumura.aki@gmail.com>
2014-10-27 20:32:49 +0900

    Railsで実装されているので不要な実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,42 +1,10 @@
 class ApplicationController < ActionController::Base
   include SessionHelper
-  
+
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_current_session
   before_action :set_operator
-  
-  # 例外ハンドル
-  if !Rails.env.development?
-    rescue_from Exception,                        with: :render_500
-    rescue_from ActiveRecord::RecordNotFound,     with: :render_404
-    rescue_from ActionController::RoutingError,   with: :render_404
-  end
-  
-  def routing_error
-    raise ActionController::RoutingError.new(params[:path])
-  end
-
-  def render_404(e = nil)
-    logger.info "Rendering 404 with exception: #{e.message}" if e
-
-    if request.xhr?
-      render json: { error: '404 error' }, status: 404
-    else
-      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
-    end
-  end
-
-  def render_500(e = nil)
-    logger.info "Rendering 500 with exception: #{e.message}" if e
-    Airbrake.notify(e) if e # Airbrake/Errbitを使う場合はこちら
-
-    if request.xhr?
-      render json: { error: '500 error' }, status: 500
-    else
-      render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
-    end
-  end
 
   protected
   def configure_permitted_parameters
@@ -45,7 +13,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :provider, :uid) }
   end
 
-  private 
+  private
 
     # model から sessionを参照可能にする
     def set_current_session
@@ -56,5 +24,5 @@ class ApplicationController < ActionController::Base
     def set_operator
       RecordWithOperator.operator = current_user
     end
-      
+
 end
