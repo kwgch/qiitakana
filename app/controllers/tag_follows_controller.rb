035568306b70e6df1a52c45664df5453f91ee7ea
Akihiro Matsumura <matsumura.aki@gmail.com>
2014-10-27 20:19:44 +0900

    controllerの使われていないメソッドにはprivateを入れる

--- app/controllers/tag_follows_controller.rb
+++ app/controllers/tag_follows_controller.rb
@@ -1,7 +1,6 @@
 class TagFollowsController < ApplicationController
-  
   before_action :set_tag
-  
+
   def create
     current_user.tag_follow!(@tag)
   end
@@ -9,9 +8,10 @@ class TagFollowsController < ApplicationController
   def destroy
     current_user.tag_unfollow!(@tag)
   end
-  
+
+private
+
   def set_tag
     @tag = Tag.find(params[:tag_follow][:tag_id])
   end
-  
 end
